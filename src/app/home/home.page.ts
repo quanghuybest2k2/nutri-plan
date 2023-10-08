bd72af7899bc81c5dc2205cf52cebc61d047614c
Doan Quang Huy <quanghuybest@gmail.com>
2023-10-08 10:15:10 +0700

    hoàn thành giao diện home, chưa có dữ liệu

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { InfoService } from '../services/info.service';
+import {
+  Exercise,
+  ExerciseCategory,
+  Categories,
+} from '../Models/exercise-category';
 
 @Component({
   selector: 'app-home',
@@ -8,24 +13,85 @@ import { InfoService } from '../services/info.service';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage implements OnInit {
-  value: number = 500;
-  // giá trị tối đa hiển thị calo tối đa
-  maxValue: number = 2000;
-
   constructor(private router: Router, private infoService: InfoService) {}
+
+  baiViet: any[] = [
+    {
+      id: 1,
+      name: 'Nên uống bao nhiêu nước 1 ngày',
+      category: 'Calories/ngày',
+      imageUrl: 'assets/NB1.png',
+    },
+    {
+      id: 1,
+      name: 'Nên uống bao nhiêu nước 1 ngày',
+      category: 'Calories/ngày',
+      imageUrl: 'assets/NB1.png',
+    },
+    {
+      id: 1,
+      name: 'Nên uống bao nhiêu nước 1 ngày',
+      category: 'Calories/ngày',
+      imageUrl: 'assets/NB1.png',
+    },
+    {
+      id: 1,
+      name: 'Nên uống bao nhiêu nước 1 ngày',
+      category: 'Calories/ngày',
+      imageUrl: 'assets/NB1.png',
+    },
+    {
+      id: 1,
+      name: 'Nên uống bao nhiêu nước 1 ngày',
+      category: 'Calories/ngày',
+      imageUrl: 'assets/NB1.png',
+    },
+    {
+      id: 1,
+      name: 'Nên uống bao nhiêu nước 1 ngày',
+      category: 'Calories/ngày',
+      imageUrl: 'assets/NB1.png',
+    },
+  ];
+
+  dinhDuong: any[] = [
+    {
+      id: 1,
+      name: 'Cơm rau củ',
+      describe: '1500 calories',
+      imageUrl: 'assets/nutri/food.png',
+    },
+    {
+      id: 1,
+      name: 'Mỳ ý sốt kem',
+      describe: '2000 calories',
+      imageUrl: 'assets/nutri/food1.png',
+    },
+    {
+      id: 1,
+      name: 'Pudding dâu',
+      describe: '200 calories',
+      imageUrl: 'assets/nutri/food2.png',
+    },
+    {
+      id: 1,
+      name: 'Mỳ xào nấm',
+      describe: '520 calories',
+      imageUrl: 'assets/nutri/food1.png',
+    },
+    {
+      id: 1,
+      name: 'Nước ép táo',
+      describe: '300 calories',
+      imageUrl: 'assets/nutri/food4.png',
+    },
+  ];
+
+  listExercises: ExerciseCategory[] = Categories;
 
   ngOnInit() {
     if (!this.infoService.isProvidedBMI()) {
       this.router.navigateByUrl('/intro');
     }
-  }
-  calculateProgress(value: number): number {
-    return Math.min((value / this.maxValue) * 100, 100);
-  }
-  calculateDashArray(value: number): string {
-    const circumference = 2 * Math.PI * 40;
-    const progress = (value / this.maxValue) * circumference;
-    const dashArray = `${progress} ${circumference}`;
-    return dashArray;
   }
 }
