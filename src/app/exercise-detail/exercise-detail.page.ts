8b19b7f024edf4b9d6fd6713b0b00782dfa95dec
Doan Quang Huy <quanghuybest@gmail.com>
2023-10-08 12:09:48 +0700

    lưu mảng count trong localstogare

--- src/app/exercise-detail/exercise-detail.page.ts
+++ src/app/exercise-detail/exercise-detail.page.ts
@@ -16,6 +16,7 @@ export class ExerciseDetailPage implements OnInit {
   selectedCategory!: ExerciseCategory;
   selectedExercise!: Exercise;
   count: number = 0;
+  countsArray: any[] = [];
 
   constructor(
     private route: ActivatedRoute,
@@ -23,10 +24,30 @@ export class ExerciseDetailPage implements OnInit {
   ) {}
 
   HanddleCount(parentId: number, exerciseId: number) {
-    this.count = this.countService.getCount(parentId, exerciseId);
+    const count = this.countService.getCounts();
+    this.countsArray = count ? [...count] : [];
+
+    // Lấy giá trị count cho bài tập hiện tại
+    const currentExerciseCount = this.countsArray.find(
+      (item) => item.parentId === parentId && item.exerciseId === exerciseId
+    );
+    this.count = currentExerciseCount ? currentExerciseCount.count : 0;
 
     // Tăng giá trị count lên 1
-    this.count = this.countService.increaseCount(parentId, exerciseId);
+    this.count++;
+
+    // Lưu giá trị count vào mảng countsArray
+    const existingIndex = this.countsArray.findIndex(
+      (item) => item.parentId === parentId && item.exerciseId === exerciseId
+    );
+    if (existingIndex !== -1) {
+      this.countsArray[existingIndex].count = this.count;
+    } else {
+      this.countsArray.push({ parentId, exerciseId, count: this.count });
+    }
+
+    // Lưu giá trị count vào local storage
+    this.countService.saveCount(parentId, exerciseId, this.count);
   }
 
   ngOnInit() {
@@ -55,7 +76,7 @@ export class ExerciseDetailPage implements OnInit {
           console.error('Không tìm thấy bài tập với ID được cung cấp.');
           return;
         }
-        // xử lý count
+        // Xử lý count
         this.HanddleCount(categoryId, exerciseId);
       } else {
         console.error('ID không hợp lệ.');
